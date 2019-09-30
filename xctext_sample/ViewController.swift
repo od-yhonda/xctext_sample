d3465117cc8151c2d959aec3b2a664b3d97bf5e1
yhonda <yhonda@opendoor.co.jp>
2019-09-30 19:26:35 +0900

    sample app

--- xctext_sample/ViewController.swift
+++ xctext_sample/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var taxTextField: UITextField!
+    @IBOutlet weak var taxLabel: UILabel!
+    @IBOutlet weak var partsTextField: UITextField!
+    @IBOutlet weak var colorTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let sampleLogic = SampleLogic()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
+private extension ViewController {
+    
+    @IBAction func didTapTaxButton() {
+        guard let text = taxTextField.text, let price = Double(text) else {
+            taxLabel.text = ""
+            return
+        }
+        let taxIncludedPrice = sampleLogic.taxIncludedPrice(price: price)
+        taxLabel.text = taxIncludedPrice.description
+    }
+    
+    @IBAction func didTapPartsButton() {
+        guard let parts = partsTextField.text else { return }
+        let message = sampleLogic.iPhone11ProBackParts(parts: parts) ? "あるよ" : "ないよ"
+        let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapImageButton() {
+        guard let color = colorTextField.text else { return }
+        imageView.image = sampleLogic.iPhone11ProImage(color: color)
+    }
+}
